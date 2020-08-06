f1f9001923ce1c380388158914981a6932cb2dd4
hongbin23 <khbasd@gmail.com>
2020-08-06 17:30:13 +0900

    test: 라인별 추가요금 단위테스트 작성

--- src/test/java/wooteco/subway/maps/map/domain/WoowaSubwaySubwayFareTest.java
+++ src/test/java/wooteco/subway/maps/map/domain/WoowaSubwaySubwayFareTest.java
@@ -2,14 +2,16 @@ package wooteco.subway.maps.map.domain;
 
 import static org.assertj.core.api.Assertions.*;
 
-import java.util.HashMap;
-import java.util.Map;
+import java.time.LocalTime;
+import java.util.Arrays;
+import java.util.List;
 
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 
-import wooteco.subway.maps.station.domain.Station;
+import wooteco.subway.maps.line.domain.Line;
 
 class WoowaSubwaySubwayFareTest {
 
@@ -17,13 +19,12 @@ class WoowaSubwaySubwayFareTest {
     @CsvSource({"9,1250", "10,1350", "15,1450", "20,1550"})
     @ParameterizedTest
     void fare(int distance, int expect) {
-        Map<Long, Station> stations = new HashMap<>();
-        stations.put(1L, new Station("2호선"));
-        stations.put(2L, new Station("3호선"));
-        stations.put(3L, new Station("4호선"));
-        stations.put(4L, new Station("5호선"));
+        List<Line> lines = Arrays.asList(
+            new Line("1호선", "red", 0, LocalTime.now(), LocalTime.now(), 10),
+            new Line("2호선", "red", 0, LocalTime.now(), LocalTime.now(), 10),
+            new Line("3호선", "red", 0, LocalTime.now(), LocalTime.now(), 10));
 
-        WoowaSubwaySubwayFare woowaSubwaySubwayFare = new WoowaSubwaySubwayFare(distance, stations);
+        WoowaSubwaySubwayFare woowaSubwaySubwayFare = new WoowaSubwaySubwayFare(distance, lines);
 
         assertThat(woowaSubwaySubwayFare.calculate()).isEqualTo(expect);
     }
@@ -32,14 +33,27 @@ class WoowaSubwaySubwayFareTest {
     @CsvSource({"50,2050", "57,2050", "58,2150"})
     @ParameterizedTest
     void fare2(int distance, int expect) {
-        Map<Long, Station> stations = new HashMap<>();
-        stations.put(1L, new Station("2호선"));
-        stations.put(2L, new Station("3호선"));
-        stations.put(3L, new Station("4호선"));
-        stations.put(4L, new Station("5호선"));
+        List<Line> lines = Arrays.asList(
+            new Line("1호선", "red", 0, LocalTime.now(), LocalTime.now(), 10),
+            new Line("2호선", "red", 0, LocalTime.now(), LocalTime.now(), 10),
+            new Line("3호선", "red", 0, LocalTime.now(), LocalTime.now(), 10));
 
-        WoowaSubwaySubwayFare woowaSubwaySubwayFare = new WoowaSubwaySubwayFare(distance, stations);
+        WoowaSubwaySubwayFare woowaSubwaySubwayFare = new WoowaSubwaySubwayFare(distance, lines);
 
         assertThat(woowaSubwaySubwayFare.calculate()).isEqualTo(expect);
+    }
+
+    @DisplayName("가장 비싼 역의 요금이 적용된다.")
+    @Test
+    void fare3() {
+        int distance = 5;
+        List<Line> lines = Arrays.asList(
+            new Line("1호선", "red", 100, LocalTime.now(), LocalTime.now(), 10),
+            new Line("2호선", "red", 200, LocalTime.now(), LocalTime.now(), 10),
+            new Line("3호선", "red", 300, LocalTime.now(), LocalTime.now(), 10));
+
+        WoowaSubwaySubwayFare woowaSubwaySubwayFare = new WoowaSubwaySubwayFare(distance, lines);
+
+        assertThat(woowaSubwaySubwayFare.calculate()).isEqualTo(1550);
     }
 }
