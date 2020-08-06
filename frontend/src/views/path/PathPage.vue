e56ed170ad1716948defe63d665afc5a0ea87db5
hongbin23 <khbasd@gmail.com>
2020-08-06 20:51:37 +0900

    feat: 현재 시간 포맷 설정

--- frontend/src/views/path/PathPage.vue
+++ frontend/src/views/path/PathPage.vue
@@ -213,7 +213,8 @@
             ...mapGetters(['stations', 'pathResult']),
             getCurrentTime() {
                 const {hour, minute} = this.departureTimeView
-                return `${hour > 12 ? '오후' : '오전'} ${hour < 10 ? `0${hour}` : hour}:${minute < 10 ? `0${minute}` : minute}`
+
+                return new Date().toLocaleTimeString([], {hour: '2-digit', minute:'2-digit'});
             }
         },
         async created() {
